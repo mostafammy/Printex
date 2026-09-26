77eab7e8cce824a5f286245478effa0027f4ea67
Mostafa Yaser <mostafa.mmy.10@gmail.com>
2026-09-26 13:35:41 +0300

    feat(auth): export public auth strategy module APIs

---
+++ src/lib/auth/index.ts
@@ -0,0 +1,6 @@
+export * from "./types";
+export * from "./validation";
+export * from "./environment";
+export * from "./strategy-factory";
+export * from "./strategies/credentials-signup-strategy";
+export * from "./strategies/github-oauth-strategy";
